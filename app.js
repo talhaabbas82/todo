9a9bd3af2047d1a99f910315bd6ef87685110255
talhaabbas82 <talhaabbas860@gmail.com>
2025-09-14 12:55:26 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+function addTodo(){
+    let todotext = document.getElementById("text").value;
+    let result = document.getElementById("result");
+    
+   
+    let ul = document.createElement("ul");
+    let li = document.createElement("li");
+    let text = document.createTextNode(todotext);
+
+    let editBtn = document.createElement("button");
+    let buttonText = document.createTextNode("edit");
+    editBtn.appendChild(buttonText);
+
+     
+    editBtn.addEventListener("click",() =>{
+        console.log("hello world!",todotext);
+        li.style.color = "green";
+        li.style.fontWeight = "bold";
+        
+    })
+    
+
+
+    let delbtn = document.createElement("button");
+    let deletext = document.createTextNode("delete");
+    delbtn.appendChild(deletext);
+
+     delbtn.addEventListener("click",()=>{
+        ul.remove();
+    })
+
+   
+    
+
+    
+    li.appendChild(text);
+    ul.appendChild(li);  
+    result.appendChild(ul);
+    li.appendChild(delbtn);
+    li.appendChild(editBtn)
+
+
+    
+
+
+}
+  
